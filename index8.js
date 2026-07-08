907fec56f838881eef79b8420f6e653b2cf089cd
KOOJOOOOO3 <jojoalrashidii@gmail.com>
2026-07-08 14:25:36 +0300

    Add files via upload

---
+++ index8.js
@@ -0,0 +1,24 @@
+const { Client, GatewayIntentBits, AttachmentBuilder } = require('discord.js');
+const client = new Client({ 
+    intents: [
+        GatewayIntentBits.Guilds, 
+        GatewayIntentBits.GuildMessages, 
+        GatewayIntentBits.MessageContent 
+    ] 
+});
+
+const TARGET_CHANNEL_ID = '1524263644630749194';
+
+client.on('messageCreate', async (message) => {
+    // التأكد من أن الرسالة في الروم المطلوب وأن المرسل ليس البوت نفسه
+    if (message.channel.id === TARGET_CHANNEL_ID && !message.author.bot) {
+        try {
+            const attachment = new AttachmentBuilder('./IMG_8637.jpg');
+            await message.channel.send({ files: [attachment] });
+        } catch (error) {
+            console.error('حدث خطأ أثناء إرسال الصورة:', error);
+        }
+    }
+});
+
+client.login(process.env.TOKEN);
